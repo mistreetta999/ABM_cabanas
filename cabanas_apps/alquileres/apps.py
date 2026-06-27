35283e3beaffef94f9ee2ed7abbfe0c303c9d783
carolinamistretta999 <tuID+mistreetta999@users.noreply.github.com>
2026-06-27 19:18:50 -0300

    trabajos 14

--- cabanas_apps/alquileres/apps.py
+++ cabanas_apps/alquileres/apps.py
@@ -1,19 +1,16 @@
 """Configuración de la aplicación de reservas y alquileres."""
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
-from django.conf import settings
-from django.utils.module_loading import autodiscover_modules
 
 class ReservasAlquileresAppsConfig(AppConfig):
-    """ class ReservasAlquileresAppsConfig representa la configuración de la aplicación de reservas y alquileres.""""
+    """ class ReservasAlquileresAppsConfig representa la configuración """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cabanas_apps.reservas_alquileres_apps'
 class AlquileresAppsConfig(AppConfig):
-    """ class AlquileresAppsConfig representa la configuración de la aplicación de alquileres.""""
+    """ class AlquileresAppsConfig representa la configuración de la aplicación de alquileres."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cabanas_apps.alquileres_apps'
 class RegistrosAppsConfig(AppConfig):
-    """ class RegistrosAppsConfig representa la configuración de la aplicación de registros.""""
+    """ class RegistrosAppsConfig representa la configuración de la aplicación de registros."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cabanas_apps.registros_apps'
-    
--- cabanas_apps/cabanas/models.py
+++ cabanas_apps/cabanas/models.py
@@ -1,14 +1,18 @@
-"""models cabana""""
+"""models cabana"""
 from django.db import models
 
 class Cabana(models.Model):
 
-"""clase cabana""
+    """clase cabana"""
     id=models.AutoField(primary_key=True) 
     nombre = models.CharField(max_length=100)
     capacidad = models.IntegerField()
     precio_por_noche = models.DecimalField(max_digits=10, decimal_places=2)
     disponible = models.BooleanField(default=True)
+    class Meta:
+        """Meta informacion para Cabana model"""
+        verbose_name = "Cabana"
+        verbose_name_plural = "Cabanas"
 
-    def __str__(self):
-        return self.nombre
+    def __str__(self:Cabana)-> str:
+        return self.nombre()
